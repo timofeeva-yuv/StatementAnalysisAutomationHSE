810bbb696966edaa698b190545f6f84ce6c918a0
Torrentov <bekyan.artem@gmail.com>
2022-12-22 20:53:44 +0300

    MVP for root table

---
+++ webpanel/login/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import CustomUser
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+
+
+# Create your views here.
+
+def index(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/')
+    error = ''
+    if request.method == "POST":
+        user = authenticate(request, username=request.POST['username'], password=request.POST['pass'])
+        if user is not None:
+            login(request, user)
+            request.session['auth'] = 1
+            return HttpResponseRedirect('/')
+        else:
+            error = "Invalid username and/or password"
+    return render(request, "login.html", {'error': error})
